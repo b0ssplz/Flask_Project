938ea87c363c9ab63844c389ec5b1c7296c2afec
b0ssplz <marcative@gmail.com>
2022-05-05 11:14:06 +0200

    bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+posts = [
+    {
+        'author': 'Marcin Wis',
+        'title': 'First post',
+        'content': 'forst post content',
+        'date_posted': '4 May 2022'
+    },
+    {
+        'author': 'Ala Pep',
+        'title': 'First post2',
+        'content': 'forst post content2',
+        'date_posted': '3 May 2022'
+    }
+    ]
+
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title = 'About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
