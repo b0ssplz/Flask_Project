198cad6adaeab7c97b3599494a3c5d47492d6299
b0ssplz <marcative@gmail.com>
2022-05-10 23:10:29 +0200

    pillow install / image add

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,5 +1,6 @@
-from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -46,16 +47,17 @@ class UpdateAccountForm(FlaskForm):
                                        Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()]) 
-    submit = SubmitField('Sign Up')
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
 
     def validate_username(self, username):
-
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('This username already exists.')
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This username already exists.')
 
     def validate_email(self, email):
-
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError('This email already exists.')
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('This email already exists.')
