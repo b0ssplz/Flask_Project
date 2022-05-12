c056accf09ba49380aa5b81fbc892c06c99b08aa
b0ssplz <marcative@gmail.com>
2022-05-12 20:38:46 +0200

    configuration

--- flaskblog/users/__init__.py
+++ flaskblog/users/__init__.py
@@ -19,7 +19,7 @@ mail = Mail(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
 
